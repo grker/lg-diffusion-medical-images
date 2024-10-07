527d5506c697988198cb353ae9c9810cd760bc32
stkramer <stkramer@ethz.ch>
2024-10-07 17:23:23 +0200

    hydra configs

--- src/utils/hydra_config.py
+++ src/utils/hydra_config.py
@@ -2,7 +2,83 @@ from dataclasses import dataclass
 from typing import Union
 
 
+class ModelConfig:
+    pass
 
-class ResNetConfig:
+class ResNetConfig(ModelConfig):
     layers: list[int]
     starting_channels: int
+
+class UNetConfig(ModelConfig):
+    sample_layers: list[int]
+    bottleneck_layers: list[int]
+    starting_channels: int
+    time_dim: int
+
+class DiffusionConfig:
+    noise_steps: int
+    beta_start: float
+    beta_end: float
+    scheduler_type: str
+    img_size: tuple[int,int]
+    device: str
+    var_learned: bool
+
+class LDSegConfig:
+    resnet: ResNetConfig
+    diffusion: DiffusionConfig
+    has_decoder: bool
+
+class DatasetConfig:
+    name: str
+    data_path: str
+    image_size: tuple[int,int]
+    normalize: bool
+    mode: str
+
+
+class DataloaderConfig:
+    batch_size: int
+    shuffle: bool
+    train_ratio: float
+    validation_ratio: float
+
+class TrainerConfig:
+    max_epochs: int
+    enable_progress_bar: bool
+    accelerator: str
+
+class SegmentationConfig:
+    project_name: str
+    dataset: DatasetConfig
+    dataloader: DataloaderConfig
+    model: ModelConfig
+    diffusion: DiffusionConfig
+    trainer: TrainerConfig
+    train: bool
+    model_path: str
+    wandb_tags: list[str]
+    seed: int
+
+class UNetConfig(ModelConfig):
+    image_size: tuple[int,int]
+    in_channels: int
+    model_channels: int
+    out_channels: int
+    num_res_blocks: int
+    attention_resolutions: list[int]
+    dropout: float
+    channel_mult: list[int]
+    conv_resample: bool
+    dims: int
+    num_classes: int
+    use_checkpoint: bool
+    use_fp16: bool
+    num_heads: int
+    num_head_channels: int
+    num_heads_upsample: int
+    use_scale_shift_norm: bool
+    resblock_updown: bool
+    use_new_attention_order: bool
+
+
