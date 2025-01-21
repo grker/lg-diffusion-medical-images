f48305d331423f8ded4de693814d0bb4aa92c690
grker <stef.kramer@bluewin.ch>
2025-01-21 17:23:50 +0100

    cuda memory logging func

--- src/utils/helper.py
+++ src/utils/helper.py
@@ -1,3 +1,5 @@
+import torch
+
 
 def unpack_batch(batch):
     images, gt_masks, gt_train_masks = None, None, None
@@ -12,3 +14,28 @@ def unpack_batch(batch):
     assert(images.shape[0] == gt_masks.shape[0] and  gt_masks.shape[0] == gt_train_masks.shape[0], "Assertion Error: images, gt_masks and gt_train_masks need to have the same number of samples")
 
     return images, gt_masks, gt_train_masks
+
+
+def log_cuda_memory(stage: str, flush: bool = False) -> None:
+    """
+    Logs the current cuda memory usage.
+    """
+    free_mem_bytes, total_mem_bytes = torch.cuda.mem_get_info()
+    free_mem = free_mem_bytes / 1_000_000_000
+    total_mem = total_mem_bytes / 1_000_000_000
+    alloc_mem = torch.cuda.memory_allocated() / 1_000_000_000
+    reserved_mem = torch.cuda.memory_reserved() / 1_000_000_000
+    max_mem_reserved = torch.cuda.max_memory_reserved() / 1_000_000_000
+    max_mem_allocated = torch.cuda.max_memory_allocated() / 1_000_000_000
+
+    print(f"\n>>>>{stage}>>>>")
+    print(
+        f"\tCurrent, max memory allocated: {alloc_mem:.2f}/{total_mem:.2f}GiB, "
+        f"{max_mem_allocated:.2f}/{total_mem:.2f}GiB"
+    )
+    print(
+        f"\tCurrent, max memory reserved: {reserved_mem:.2f}/{total_mem:.2f}GiB, "
+        f"{max_mem_reserved:.2f}/{total_mem:.2f}GiB"
+    )
+    print(f"\tCurrent memory free: {free_mem:.2f}/{total_mem:.2f} GiB")
+    print(f"<<<<{stage}<<<<")
