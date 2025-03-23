2ba9ca6733544b0dcab34b153f20edd9c931ee51
stkramer <stkramer@ethz.ch>
2025-03-23 23:25:18 +0100

    Topoloss

--- src/loss/__init__.py
+++ src/loss/__init__.py
@@ -1,9 +1,11 @@
 from .birth_death_loss import BirthDeathIntervalLoss, BirthDeathLoss
 from .loss import CustomLoss, single_loss_fn
+from .topo_loss import TopoLoss
 
 __all__ = [
     "CustomLoss",
     "BirthDeathIntervalLoss",
     "BirthDeathLoss",
     "single_loss_fn",
+    "TopoLoss",
 ]
